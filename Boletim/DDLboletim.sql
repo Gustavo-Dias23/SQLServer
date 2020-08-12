e66c717f2c2d536048b6ce34021401cb0445d9b3
Gustavo Dias <dias.gustavo23@gmail.com>
2020-08-12 10:56:37 -0300

    DD

---
+++ Boletim/DDLboletim.sql
@@ -0,0 +1,35 @@
+/*	DDL  */
+
+CREATE DATABASE boletim;
+
+/* Usamos o banco efetivamente */
+USE boletim;
+GO
+
+/* Excluir o banco */
+/* DROP DATABASE boletim; */
+
+/* Criada a tabela Aluno */
+/* IDENTITY -> Gera os IDs automaticamente de forma sequencial*/
+CREATE TABLE aluno (
+	idAluno INT IDENTITY PRIMARY KEY NOT NULL,
+	Nome VARCHAR(100) NOT NULL,
+	Ra VARCHAR(20),
+	Idade INT 
+);
+
+/* Criada a tabela Materia */
+CREATE TABLE materia (
+	IdMateria INT PRIMARY KEY IDENTITY NOT NULL,
+	Titulo VARCHAR(50) NOT NULL,
+);
+
+/* Criada a tabela Trabalho */
+CREATE TABLE trabalho (
+	IdTrabalho INT PRIMARY KEY IDENTITY NOT NULL,
+	Nota DECIMAL,
+
+	-- Chamamos as chaves estrangeiras (Foreign Keys)
+	IdAluno INT FOREIGN KEY REFERENCES aluno (IdAluno),
+	IdMateria INT FOREIGN KEY REFERENCES materia (IdMateria)
+);
