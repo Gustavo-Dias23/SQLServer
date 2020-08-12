f7163a99c359648c1a143e7b85434f556fe6c5e6
Gustavo Dias <dias.gustavo23@gmail.com>
2020-08-12 14:23:10 -0300

    DQLs adicionadas(SELECT)

---
+++ Boletim/DQLBoletim.sql
@@ -0,0 +1,26 @@
+USE boletim;
+
+/* DQL - Data Query Language */
+-- Selecionar todos os dados da tabela
+SELECT * FROM Aluno;
+
+--Selecionar um dado específico
+SELECT * FROM Aluno WHERE IdAluno = 1 OR IdAluno = 2;
+
+-- Selecionar como uma busca específica
+SELECT * FROM Aluno WHERE Nome LIKE '%An%' AND Ra LIKE '%35%';
+
+-- Ordenação por forma crescente (padrão)
+SELECT * FROM Aluno ORDER BY Nome;
+
+-- Ordenação por forma crescente (crescente)
+SELECT * FROM Aluno ORDER BY Nome ASC;
+
+-- Ordenação por forma crescente (decrescente)
+SELECT * FROM Aluno ORDER BY Nome DESC;
+
+-- Selecionar dados com condições especiais
+SELECT * FROM Aluno WHERE IdAluno > 1 AND IdAluno < 5;
+
+-- Selecionar dados entre valores específicos
+SELECT * FROM Trabalho WHERE DataEntrega BETWEEN '2020-08-05T00:00:00' AND '2020-08-15T23:00:00'
---
+++ Optus/DQLOptus.sql
@@ -0,0 +1,7 @@
+USE musica
+
+SELECT * FROM estilo;
+
+SELECT * FROM album WHERE DataLacamento BETWEEN '2013-01-01T00:00:00' AND '2015-01-01T00:00:00'
+
+SELECT * FROM artista  WHERE IdArtista >0 AND IdArtista <2;
